b0b17bceaf4051d6d0838c70f1c292e9904854c2
Murphyzzz <31411653+Murphyzzz@users.noreply.github.com>
2022-01-08 14:07:31 +0800

    Update constant.py

--- constant.py
+++ constant.py
@@ -11,13 +11,13 @@ REPORT_API = 'https://app.bupt.edu.cn/ncov/wap/default/save'
 # 当今日没有填报时，在https://app.bupt.edu.cn/ncov/wap/default/index下进行填报，
 # 全部填完，不要提交，f12打开控制台，在Console页面下输入代码 console.log(vm.info) 就会得到以下信息，之后每天就默认填以下信息
 INFO = r"""{
-        "address":"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区",
+        "address":"北京市海淀区西三旗街道君安家园(西区)君安家园西区",
         "area":"北京市  海淀区",
         "bztcyy":"",
         "city":"北京市",
         "csmjry":"0",
         "fjqszgjdq":"",
-        "geo_api_info":"{\"type\":\"complete\",\"position\":{\"Q\":39.960390625,\"R\":116.356397569445,\"lng\":116.356398,\"lat\":39.960391},\"location_type\":\"html5\",\"message\":\"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.\",\"accuracy\":23,\"isConverted\":true,\"status\":1,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110108\",\"businessAreas\":[{\"name\":\"北下关\",\"id\":\"110108\",\"location\":{\"Q\":39.955976,\"R\":116.33873,\"lng\":116.33873,\"lat\":39.955976}},{\"name\":\"西直门\",\"id\":\"110102\",\"location\":{\"Q\":39.942856,\"R\":116.34666099999998,\"lng\":116.346661,\"lat\":39.942856}},{\"name\":\"小西天\",\"id\":\"110108\",\"location\":{\"Q\":39.957147,\"R\":116.364058,\"lng\":116.364058,\"lat\":39.957147}}],\"neighborhoodType\":\"科教文化服务;学校;高等院校\",\"neighborhood\":\"北京邮电大学\",\"building\":\"北京邮电大学计算机学院\",\"buildingType\":\"科教文化服务;学校;高等院校\",\"street\":\"西土城路\",\"streetNumber\":\"10号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"海淀区\",\"township\":\"北太平庄街道\"},\"formattedAddress\":\"北京市海淀区北太平庄街道北京邮电大学计算机学院北京邮电大学海淀校区\",\"roads\":[],\"crosses\":[],\"pois\":[],\"info\":\"SUCCESS\"}",
+        "geo_api_info": "{\"type\":\"complete\",\"info\":\"SUCCESS\",\"status\":1,\"fEa\":\"jsonp_462438_\",\"position\":{\"Q\":40.04175,\"R\":116.34296,\"lng\":116.34296,\"lat\":40.04175},\"message\":\"Get ipLocation success.Get address success.\",\"location_type\":\"ip\",\"accuracy\":null,\"isConverted\":true,\"addressComponent\":{\"citycode\":\"010\",\"adcode\":\"110108\",\"businessAreas\":[{\"name\":\"西三旗\",\"id\":\"110108\",\"location\":{\"Q\":40.05349,\"R\":116.35109599999998,\"lng\":116.351096,\"lat\":40.05349}},{\"name\":\"清河\",\"id\":\"110108\",\"location\":{\"Q\":40.040177,\"R\":116.34281499999997,\"lng\":116.342815,\"lat\":40.040177}},{\"name\":\"永泰\",\"id\":\"110108\",\"location\":{\"Q\":40.03475,\"R\":116.35658000000001,\"lng\":116.35658,\"lat\":40.03475}}],\"neighborhoodType\":\"商务住宅;住宅区;住宅小区\",\"neighborhood\":\"君安家园(西区)\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"营福路\",\"streetNumber\":\"9号\",\"country\":\"中国\",\"province\":\"北京市\",\"city\":\"\",\"district\":\"海淀区\",\"towncode\":\"110108019000\",\"township\":\"西三旗街道\"},\"formattedAddress\":\"北京市海淀区西三旗街道君安家园(西区)君安家园西区\",\"roads\":[],\"crosses\":[],\"pois\":[]}",
         "glksrq":"",
         "gllx":"",
         "gtjzzchdfh":"",
